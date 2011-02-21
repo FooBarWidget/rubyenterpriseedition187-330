d03a4556ae599ed4f8e6129157d58454bc36ab56
Hongli Lai (Phusion) <hongli@phusion.nl>
2011-02-21 10:56:53 +0100

    Bump version to 2011.02.

--- version.c
+++ version.c
@@ -26,7 +26,7 @@ const int ruby_patchlevel = RUBY_PATCHLEVEL;
 const char *ruby_description;
 const char *ruby_copyright;
 
-#define REE_VERSION "2011.01"
+#define REE_VERSION "2011.02"
 
 void
 Init_version()
